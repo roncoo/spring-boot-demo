433536b04c01f1a75db024fa1018aa17fd56c3ee
wujing <297115770@qq.com>
2016-08-22 14:44:13 +0800

    课程2

---
+++ spring-boot-demo-2-1/src/main/java/com/roncoo/education/SpringBootDemo21Application.java
@@ -0,0 +1,12 @@
+package com.roncoo.education;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDemo21Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDemo21Application.class, args);
+	}
+}
