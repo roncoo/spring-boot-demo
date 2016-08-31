51e8b522a38357f1db81f66faa62085bb9b42033
wujing <297115770@qq.com>
2016-08-31 18:34:59 +0800

    日志配置-log4j2配置方式

---
+++ spring-boot-demo-5-2/src/main/java/com/roncoo/education/SpringBootDemo52Application.java
@@ -0,0 +1,12 @@
+package com.roncoo.education;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDemo52Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDemo52Application.class, args);
+	}
+}
