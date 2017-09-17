0132346b28ca1fdc972d93f00ae5a8612e5ca54e
wujing <297115770@qq.com>
2017-09-17 10:33:14 +0800

    使用NoSQL数据库-mongodb

---
+++ spring-boot-demo-18-1/src/main/java/com/roncoo/example/util/configuration/CustomCorsConfiguration.java
@@ -0,0 +1,21 @@
+package com.roncoo.example.util.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class CustomCorsConfiguration {
+	
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurerAdapter() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				//registry.addMapping("/api/**").allowedOrigins("http://localhost:8080");
+			}
+		};
+	}
+}
