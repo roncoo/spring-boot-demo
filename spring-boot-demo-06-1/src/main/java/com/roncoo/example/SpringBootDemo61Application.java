3f13b9be61804ed144381f9c362943f62280456c
wujing <297115770@qq.com>
2017-09-17 10:08:56 +0800

    WEB应用开发-模板引擎FreeMarker

---
+++ spring-boot-demo-06-1/src/main/java/com/roncoo/example/SpringBootDemo61Application.java
@@ -0,0 +1,12 @@
+package com.roncoo.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDemo61Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDemo61Application.class, args);
+	}
+}
---
+++ spring-boot-demo-06-1/src/main/java/com/roncoo/example/controller/WebController.java
@@ -0,0 +1,22 @@
+package com.roncoo.example.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/web")
+public class WebController {
+	
+	private static final Logger logger =  LoggerFactory.getLogger(WebController.class);
+	
+	@RequestMapping("index")
+	public String index(ModelMap map){
+		logger.info("这里是controller");
+		map.put("title", "hello world");
+		return "index"; // 注意，不要在最前面加上/，linux下面会出错
+	}
+
+}
